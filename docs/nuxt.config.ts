b11234b388b10ab523daf78b200ec7fdbd5ee9f8
Dan Pastori <dan@521dimensions.com>
2025-10-06 12:01:47 -0500

    Removed baseurl

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -19,9 +19,6 @@ export default defineNuxtConfig({
       indexable: process.env.NUXT_SITE_ENV === 'production',
       trailingSlash: true
     },
-    app: {
-      baseURL: '/open-source/amplitudejs',
-    },
     sitemap: {
       sitemaps: {
         pages: {
